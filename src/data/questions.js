0d06a18fc09efdc99ac4eb4fe556486f2debc69d
Ian Feather <info@ianfeather.co.uk>
2018-07-31 16:39:54 +0100

    Basics

---
+++ src/data/questions.js
@@ -0,0 +1,27 @@
+export const questions = [
+  {
+    text: "Pick a price point",
+    answers: [
+      "$",
+      "$$",
+      "$$$"
+    ]
+  },
+  {
+    text: "Pick an interest",
+    answers: [
+      "Culture",
+      "Food",
+      "Outdoors"
+    ]
+  },
+  {
+    text: "Vibe",
+    answers: [
+      "City",
+      "Small Town",
+      "Remote"
+    ]
+  },
+]
+
