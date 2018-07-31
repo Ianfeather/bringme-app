202b63a6a209428871e240d15c8864b2cdaff42d
Ian Feather <info@ianfeather.co.uk>
2018-07-31 18:12:22 +0100

    Some styling

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import App from './App.html';
 import { questions } from './data/questions.js';
 import { Store } from 'svelte/store.js';
 
-const url = 'https://gist.githubusercontent.com/ben-graves/5b8fe403b6a07239cb1dab2faacdf9a7/raw/f05efd6eeb66f0a6b522b7be3d5b12283099e162/bringme.json';
+const url = 'https://gist.githubusercontent.com/ben-graves/5b8fe403b6a07239cb1dab2faacdf9a7/raw/cb0657ed9bee51b88485e53fa1971d1b209d605c/bringme.json';
 
 fetch(url)
     .then(res => res.ok ? res.json() : Promise.reject(res))
